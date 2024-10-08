d17c21b55021252a895292f50a5367c1b2ab2d62
GSmithApps <14.gsmith.14@gmail.com>
2024-10-07 21:48:10 -0500

    pretty darn good

--- home.nix
+++ home.nix
@@ -106,8 +106,9 @@
   programs.kitty = {
     enable = true;
     settings = {
-      background_opacity = "0.8";
+      background_opacity = "0.9";
       font_family = "FiraCode Nerd font";
+      font_size = 17;
     };
   };
 
