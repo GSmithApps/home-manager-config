25c419730bfbe0f05e97354c9b6884878fa79b66
GSmithApps <14.gsmith.14@gmail.com>
2024-10-17 19:17:52 -0500

    new flake.nix with my packages

--- flake.nix
+++ flake.nix
@@ -23,12 +23,123 @@
                 homeDirectory = "/home/grants";
                 stateVersion = "24.05";
                 packages = with pkgs; [
+                  # # You can also create simple shell scripts directly inside your
+                  # # configuration. For example, this adds a command 'my-hello' to your
+                  # # environment:
+                  # (pkgs.writeShellScriptBin "my-hello" ''
+                  #   echo "Hello, ${config.home.username}!"
+                  # '')
                   lazygit
+                  fira-code-nerdfont
+                  obsidian
+                  pdfarranger
+                  qpdf
+                  wl-clipboard
+                  #nvchad
+                    ripgrep
+                    gcc13
+                    gnumake
+                  #java
+                    # zulu17
+                  (pkgs.vscode-with-extensions.override {
+                    vscodeExtensions = with pkgs.vscode-extensions; [
+                      jnoortheen.nix-ide
+                      eamodio.gitlens
+                      mhutchie.git-graph
+                      mechatroner.rainbow-csv
+                      hediet.vscode-drawio
+
+                      # java things
+                        # vscjava.vscode-java-pack
+                        # vscjava.vscode-gradle
+                        # redhat.java
+                        # vscjava.vscode-java-test
+                        # vscjava.vscode-java-debug
+                        # vscjava.vscode-spring-initializr
+                    ];
+                  })
                 ];
+
+
+                home.file = {
+
+                  ".config/Code/User/settings.json".text = builtins.readFile ./vscode.json;
+
+                  ".config/nvim" = {
+                    source = pkgs.fetchFromGitHub {
+                      owner = "NvChad";
+                      repo = "starter";
+                      rev = "main";
+                      # sha256 = "sha256-d0c602f5f155d4d1261609219e9b8a61e936d681";
+                      # sha256 = lib.fakeSha256;
+                      sha256 = "sha256-SVpep7lVX0isYsUtscvgA7Ga3YXt/2jwQQCYkYadjiM";
+                    };
+                    recursive = true;
+                  };
+                };
+
+                home.sessionVariables = {
+                  EDITOR = "nvim";
+                  DIRENV_LOG_FORMAT="";
+                };
               };
               nixpkgs.config.allowUnfree = true;
               programs = {
-                home-manager.enable = true;
+
+                # Let Home Manager install and manage itself.
+                home-manager = {
+                  enable = true;
+                };
+
+                zsh = {
+                  enable = true;
+                  oh-my-zsh = {
+                    enable = true;
+                    theme = "agnoster";
+                    plugins = [
+                      "git"
+                      "direnv"
+                    ];
+                  };
+                };
+
+                git = {
+                  enable = true;
+                  userName = "GSmithApps";
+                  userEmail = "14.gsmith.14@gmail.com";
+                };
+
+                kitty = {
+                  enable = true;
+                  settings = {
+                    background_opacity = "0.98";
+                    font_family = "FiraCode Nerd font";
+                    font_size = 17;
+                  };
+                };
+
+                direnv = {
+                  enable = true;
+                  enableZshIntegration = true;
+                  nix-direnv.enable = true;
+                };
+
+                alacritty = {
+                  enable = true;
+                  settings = {
+                    window.opacity = 0.98;
+                    font.normal.family = "FiraCode Nerd font";
+                    font.size = 20;
+                  };
+                };
+
+                tmux = {
+                  enable = true;
+                };
+
+                neovim = {
+                  enable = true;
+                };
               };
             }
           ];
